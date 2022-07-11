284717b6b14f3a61f31d3bec29983a7d567b2d20
Angel Concha Layme <48967493+Angel1612@users.noreply.github.com>
2022-07-11 14:25:44 -0500

    Update main.cpp

--- Laboratorio_08_parte_3/main.cpp
+++ Laboratorio_08_parte_3/main.cpp
@@ -204,7 +204,7 @@ int main(){
 
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
+            float angle = 180.0f * i;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
 
@@ -231,26 +231,3 @@ void processInput(GLFWwindow* window){
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
