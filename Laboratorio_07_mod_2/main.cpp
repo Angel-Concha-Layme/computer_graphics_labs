933e749ed7e65ecbea866b1834bca2868aea77f4
Angel Concha Layme <48967493+Angel1612@users.noreply.github.com>
2022-07-11 21:37:59 -0500

    Update main.cpp

--- Laboratorio_07_mod_2/main.cpp
+++ Laboratorio_07_mod_2/main.cpp
@@ -578,7 +578,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//glfw window creation
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "my version opengl", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Proyecto transformaciones", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create glfw window" << std::endl;
